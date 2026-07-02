93f5171760002b8fd72de9504a9b9c4d3b05f8ba
Aditya Biswal <aditya812007@gmail.com>
2026-07-02 17:14:31 +0530

    Object Oriented Programming Part 1

---
+++ day12/student.py
@@ -0,0 +1,14 @@
+def main():
+    name  = get_name()
+    house = get_house()
+    print(f"{name} from {house}")
+
+def get_name():
+    return input("name: ")
+
+def get_house():
+    return input("house: ")
+
+if __name__ == "__main__":
+    main()
+     
